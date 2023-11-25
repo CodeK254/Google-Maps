02a9c2754c17ffd9494ad7fd059fa585fff46470
CodeK254 <codekarma1003@gmail.com>
2023-11-25 11:01:22 +0300

    Commit 0.0.2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,6 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // GMSServices.provideAPIKey("AIzaSyBTZizk55OSYSqUr9cE2dxUnTe4GSsvWFE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
